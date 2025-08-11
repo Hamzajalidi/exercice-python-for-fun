54a5a3e7d94cd524ed4a190d961ef4371cc713c7
hamza jalidi <hamzajalidi21@gmail.com>
2025-08-11 11:51:24 +0100

    Add files via upload

---
+++ ex10.py
@@ -0,0 +1,7 @@
+import math
+xa = int(input("enter la valeur de xa :"))
+ya = int(input("enter la valeur de ya :"))
+xb = int(input("enter la valeur de xb :"))
+yb = int(input("enter la valeur de yb :"))
+ab = math.sqrt((xb-xa)**2 + (yb-ya)**2)
+print("la distance enter a et b",ab)
